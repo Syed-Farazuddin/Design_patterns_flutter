3082d316026e3193cb095cbf197307943eed1770
Syed-Farazuddin <syedfarazuddin73@gmail.com>
2024-08-23 22:33:14 +0530

    state management in progress

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:learning/chopper_api/service.dart';
 import 'package:learning/design_patterns/factory_method/factory_main.dart';
 import 'package:learning/model/model.dart';
 import 'package:learning/services/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
